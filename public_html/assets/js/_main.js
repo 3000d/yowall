7fe5d56bc05c00d94d9c713bfde132980469710b
Jonathan Berger <drskullster@gmail.com>
2014-08-08 18:32:05 +0200

    bugfix in color comparison

--- public_html/assets/js/_main.js
+++ public_html/assets/js/_main.js
@@ -4,22 +4,20 @@ $(function() {
 
   $("#add").on('click', function() {
     var color = generateColorNumber();
-    console.log('yo: ' + color, old_color);
-    while(color === old_color) {
-      color = generateColorNumber();
-    }
+
     while(color === old_color || color === old_old_color) {
       color = generateColorNumber();
     }
+
     var $elmt = $('<li/>').addClass('color_' + color).addClass('new').html('markus');
     $("#yo_list").prepend($elmt);
-    $('#yo_list').scrollTo(0, 400);
+
     setTimeout(function() {
       $elmt.removeClass('new');
     }, 100);
 
-    old_color = color;
     old_old_color = old_color;
+    old_color = color;
   });
 
   function generateColorNumber() {
