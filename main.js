7b35778efc4735b64913d351282d0e01b08bfa16
Greg Berger <greg@3kd.be>
2014-08-08 18:33:50 +0200

    Server 0.1

--- main.js
+++ main.js
@@ -1,14 +1,50 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.configure(function(){
-  app.use('/', express.static(__dirname + '/' + Settings.clientDirectory));
+var request = require('request');
+
+
+
+
+var server = require('http').createServer(app).listen(3000);
+  var io = require('socket.io').listen(server);
+
+app.use('/',express.static(__dirname + '/public_html'));
+
+
+app.get('/yo', function(req, resp){
+  var username=req.query.username;
+  var options = {
+    host:'api.justyo.co',
+    path:"/yo",
+    method:"post"
+  };
+  request.post(
+    'http://api.justyo.co/yo',
+    {form:
+      {
+        api_token:process.env.YO_API_KEY,
+        username: username// 'LABS3KD'
+      }
+    },
+    function(error,response,body){
+      resp.send(body.result);
+      if(! error && response.statusCode == 200){
+
+      }
+    }
+  );
+  console.log('trying to yo '+username);
+//  resp.send('yo');
 });
 
-var server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+app.get('/receiveyo',function(req,resp){
+  var username = req.query.username;
 
-server.listen(Settings.serverPort);
+  resp.send('ok');
+});
+
+// server.listen(3000);
 
 io.sockets.on('connection', function (socket) { // connection is initialized
   console.log("connected"); 
@@ -18,4 +54,5 @@ io.sockets.on('connection', function (socket) { // connection is initialized
   socket.on('cmd', function (data) { // received "cmd" from client
     console.log(data);
   });
-};
+
+});
