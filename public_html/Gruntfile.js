ad1582613bbb6b446c8fabe552ab9010727dd71b
Jonathan Berger <drskullster@gmail.com>
2014-08-08 16:30:25 +0200

    front bootstrap

---
+++ public_html/Gruntfile.js
@@ -0,0 +1,130 @@
+'use strict';
+module.exports = function(grunt) {
+  // Load all tasks
+  require('load-grunt-tasks')(grunt);
+  // Show elapsed time
+  require('time-grunt')(grunt);
+
+  var jsFileList = [
+    'assets/js/plugins/*.js',
+    'assets/js/_*.js'
+  ];
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        '!assets/js/scripts.js',
+        '!assets/**/*.min.*'
+      ]
+    },
+    less: {
+      dev: {
+        files: {
+          'assets/css/main.css': [
+            'assets/less/main.less'
+          ]
+        },
+        options: {
+          compress: false,
+          sourceMap: true,
+          sourceMapFilename: 'assets/css/main.css.map',
+          sourceMapRootpath: '/'
+        }
+      },
+      build: {
+        files: {
+          'assets/css/main.min.css': [
+            'assets/less/main.less'
+          ]
+        },
+        options: {
+          compress: true
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+        sourceMap: true
+      },
+      dist: {
+        src: [jsFileList],
+        dest: 'assets/js/scripts.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [jsFileList]
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
+      },
+      dev: {
+        options: {
+          map: {
+            prev: 'assets/css/'
+          }
+        },
+        src: 'assets/css/main.css'
+      },
+      build: {
+        src: 'assets/css/main.min.css'
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'assets/less/*.less',
+          'assets/less/**/*.less'
+        ],
+        tasks: ['less:dev', 'autoprefixer:dev']
+      },
+      js: {
+        files: [
+          jsFileList,
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'concat']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: true
+        },
+        files: [
+          'assets/css/main.css',
+          'assets/js/scripts.js',
+          '*.php'
+        ]
+      }
+    }
+  });
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'dev'
+  ]);
+  grunt.registerTask('dev', [
+    'jshint',
+    'less:dev',
+    'autoprefixer:dev',
+    'concat'
+  ]);
+  grunt.registerTask('build', [
+    'jshint',
+    'less:build',
+    'autoprefixer:build',
+    'uglify',
+    'modernizr'
+  ]);
+};
