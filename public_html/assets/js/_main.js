8e639aad3ffd029a2ec1a1e0ee39700ee0a80b9f
Jonathan Berger <drskullster@gmail.com>
2014-08-08 18:41:15 +0200

    added socket magic

--- public_html/assets/js/_main.js
+++ public_html/assets/js/_main.js
@@ -1,15 +1,25 @@
 $(function() {
+  var socket = io.connect();
+
   var old_color = -1;
   var old_old_color = -1;
 
-  $("#add").on('click', function() {
+  $("#add").on('click', function () {
+    addYo(Math.random().toString(36).substring(7));
+  });
+
+  socket.on('yo', function (data) {
+    addYo(data.username);
+  });
+
+  function addYo(username) {
     var color = generateColorNumber();
 
     while(color === old_color || color === old_old_color) {
       color = generateColorNumber();
     }
 
-    var $elmt = $('<li/>').addClass('color_' + color).addClass('new').html('markus');
+    var $elmt = $('<li/>').addClass('color_' + color).addClass('new').html(username);
     $("#yo_list").prepend($elmt);
 
     setTimeout(function() {
@@ -18,7 +28,7 @@ $(function() {
 
     old_old_color = old_color;
     old_color = color;
-  });
+  }
 
   function generateColorNumber() {
     return parseInt(Math.random() * 7);
